2c620cac283e24601d248d1ee36d257a828b7884
Mike Rybka <merybka@gmail.com>
2023-12-14 14:28:28 +0000

    wip

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/mikerybka/infra"
@@ -12,7 +13,7 @@ import (
 
 func main() {
 	cmds := []string{}
-	f, err := os.Open(filepath.Join(util.HomeDir(), "src/cmds.json"))
+	f, err := os.Open(filepath.Join(util.HomeDir(), "src/github.com/mikerybka/cmds.json"))
 	if err != nil {
 		log.Fatalf("no cmd.json file")
 	}
@@ -21,10 +22,28 @@ func main() {
 		log.Fatalf("unable to parse cmds.json file: %s", err)
 	}
 	for _, pkg := range cmds {
-		outDir := filepath.Join(util.HomeDir(), "src/github.com/mikerybka/builds/main")
+		outDir := filepath.Join(util.HomeDir(), "src/github.com/mikerybka/builds.mikerybka.com/public/main")
 		err := infra.Build(pkg, outDir)
 		if err != nil {
 			log.Fatalf("Error building %s: %s", pkg, err)
 		}
+	}
+	cmd := exec.Command("git", "add", "--all")
+	cmd.Dir = filepath.Join(util.HomeDir(), "src/github.com/mikerybka/builds.mikerybka.com")
+	err = util.Run(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd = exec.Command("git", "commit", "-m", "wip")
+	cmd.Dir = filepath.Join(util.HomeDir(), "src/github.com/mikerybka/builds.mikerybka.com")
+	err = util.Run(cmd)
+	if err != nil {
+		return
+	}
+	cmd = exec.Command("git", "push")
+	cmd.Dir = filepath.Join(util.HomeDir(), "src/github.com/mikerybka/builds.mikerybka.com")
+	err = util.Run(cmd)
+	if err != nil {
+		panic(err)
 	}
 }
